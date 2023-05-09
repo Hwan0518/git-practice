bff33635da354d9f90325cab9b0429136cd283fe
Hwan0518 <qip0518@naver.com>
2023-05-09 21:51:17 +0900

    feat: Print fizz & buzz & fizzbuzz
    Condition : i is multiple of 3. then print "fizz"
    	    i is multiple of 5. then print "buzz"
    	    i is multiple of 15. then print "fizzbuzz"
    Otherwise : print "i"

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-def do_fizzbuzz():
+def do_fizzbuzz(num):
 	"""
 	fizzbuzz : print fizz buzz fizzbuzz
 	3 : fizz
@@ -6,7 +6,16 @@ def do_fizzbuzz():
 	15 : fizzbuzz
 	etc : num
 	"""
-	print('hello')
+	for i in range(1,num+1):
+		if i%15 == 0:
+			print("fizzbuzz")
+		elif i%5 == 0:
+			print("buzz")
+		elif i%3 ==0:
+			print("fizz")
+		else:
+			print(i)
+
 
 if __name__ =='__main__':
-	do_fizzbuzz()
+	do_fizzbuzz(16)
